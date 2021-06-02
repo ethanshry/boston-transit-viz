81968cb42c057488abf135ad593875b19bbe9916
ethanshry <ehshry@gmail.com>
2021-06-02 15:17:30 -0400

    working on baseline map app

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+  export let name;
+
+  let count = 0;
+  let showMap = false;
+
+  $: document.title = `${name} ${count}`;
+
+  import Map from "./Map.svelte";
+
+  function getNewMapData() {
+    console.log("fetching...");
+    count++;
+  }
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <button on:click={getNewMapData}>Refresh Map</button>
+  <button on:click={() => (showMap = true)}>Display</button>
+  <Map lineName="MTBA System" {showMap} />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/Map.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let lineName;
+  export let showMap;
+</script>
+
+<map>
+  {#if showMap}
+    <h2>Map of the {lineName}</h2>
+    <canvas width="500" height="500" />
+  {:else}
+    <h2>Click to display the map</h2>
+  {/if}
+</map>
+
+<style>
+  canvas {
+    width: 100%;
+    height: 100%;
+  }
+</style>

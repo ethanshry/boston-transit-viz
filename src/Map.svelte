659bc0257d3dc4788792d1edf9838c7bf2557aca
ethanshry <ehshry@gmail.com>
2021-06-07 23:36:58 -0400

    map is drawing lines of routes, animation in progress

--- src/Map.svelte
+++ src/Map.svelte
@@ -4,17 +4,87 @@
   export let routes;
   let canvas;
 
-  let canvasDimensions = {
-    minLat: 42,
-    maxLat: 43,
-    minLong: -70.5,
-    maxLong: -71.5,
+  export let canvasDimensions = {
+    x: 800,
+    y: 800,
   };
+
+  let canvasMapCoordinates = {
+    //minLat: 42,
+    //maxLat: 42.8,
+    //minLong: -70.6,
+    //maxLong: -71.4,
+    minLat: 42.3,
+    maxLat: 42.5,
+    minLong: -70.9,
+    maxLong: -71.1,
+  };
+
+  let positionFetchDelayMillis = 5000;
+  $: animationUpdateDelayMillis = (positionFetchDelayMillis / 1000) * 60;
+
+  function getCanvasPosition(position) {
+    let x =
+      ((position.latitude - canvasMapCoordinates.minLat) /
+        (canvasMapCoordinates.maxLat - canvasMapCoordinates.minLat)) *
+      canvasDimensions.x;
+    let y =
+      ((position.longitude - canvasMapCoordinates.minLong) /
+        (canvasMapCoordinates.maxLong - canvasMapCoordinates.minLong)) *
+      canvasDimensions.y;
+    return { x, y };
+  }
+
+  function getAnimationSteps(pointA, pointB, steps) {
+    let minSegmentDist = 1;
+    if (!pointA || !pointB) return;
+    // Else, interpolate points along the line
+    let deltaX = pointB.latitude - pointA.latitude;
+    let deltaY = pointB.longitude - pointA.longitude;
+    let distTotal = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    let propX = deltaX / distTotal;
+    let propY = deltaY / distTotal;
+    let segmentDist =
+      distTotal / steps > minSegmentDist ? distTotal / steps : minSegmentDist;
+
+    let waypoints = [];
+    while (
+      Math.abs(pointA.latitude - pointB.latitude) +
+        Math.abs(pointA.longitude - pointB.longitude) >
+      0
+    ) {
+      let point = {
+        latitude: pointA.latitude + segmentDist * propX,
+        longitude: pointA.longitude + segmentDist * propY,
+      };
+      if (
+        (pointA.latitude > pointB.latitude &&
+          point.latitude < pointB.latitude) ||
+        point.latitude >= pointB.latitude
+      ) {
+        point.latitude = pointB.latitude;
+      }
+      if (
+        (pointA.longitude > pointB.longitude &&
+          point.longitude < pointB.longitude) ||
+        point.longitude >= pointB.longitude
+      ) {
+        point.longitude = pointB.longitude;
+      }
+      waypoints.push(point);
+      pointA = point;
+    }
+    return waypoints;
+  }
 
   $: ctx = initCanvas(canvas);
 
   function initCanvas(cvs) {
     let ctx = cvs?.getContext("2d");
+    if (ctx) {
+      // TODO maybe fix the blur??
+      //ctx.imageSmoothingEnabled = false;
+    }
     // ctx?.translate(cvs?.width / 2, cvs?.height / 2);
     //ctx.scale(1, -1);
     return ctx;
@@ -53,43 +123,80 @@
 
   setInterval(async () => {
     if (showMap) {
+      // Get most recent vehicle position data
       let positionData = await getVehiclePositions();
       positionData.forEach((v) => {
         if (data[v.id] !== undefined) {
-          data[v.id] = [...data[v.id], v];
+          data[v.id].points = [
+            ...data[v.id].points,
+            { latitude: v.latitude, longitude: v.longitude },
+          ];
+          data[v.id].animationTargets = getAnimationSteps(
+            data[v.id].points[data[v.id].points.length - 1],
+            data[v.id].points[data[v.id].points.length - 2],
+            animationUpdateDelayMillis
+          );
         } else {
-          data[v.id] = [v];
+          data[v.id] = {
+            route: v.route,
+            points: [{ latitude: v.latitude, longitude: v.longitude }],
+            animationTargets: [],
+          };
         }
       });
     } else {
+      // We don't want to show the map, so clear the data
       data = {};
     }
-    // then we need to draw the map?
-    for (let k in data) {
-      let pos = data[k][data[k].length - 1];
-      // color defaults to black
-      let color = routes[pos.route]?.color;
-      console.log(color);
+    console.log(data);
+  }, positionFetchDelayMillis);
+
+  // Map Drawing Loop
+  setInterval(() => {
+    if (!ctx) return;
+    ctx.clearRect(0, 0, canvasDimensions.x, canvasDimensions.y);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvasDimensions.x, canvasDimensions.y);
+    for (let key in data) {
+      let vehicle = data[key];
+      let color = routes[vehicle.route]?.color;
+      let pos = getCanvasPosition(vehicle.points[data[key].points.length - 1]);
+      // draw the line up to the endpoint
       ctx.fillStyle = color;
       ctx.strokeStyle = color;
       ctx.beginPath();
-      ctx.arc(
-        (pos.latitude - canvasDimensions.minLat) * 1000,
-        (pos.longitude - canvasDimensions.minLong) * 1000 * -1,
-        1,
-        0,
-        2 * Math.PI
-      );
-      ctx.fill();
+      if (vehicle.points.length < 1) {
+        continue;
+      }
+      ctx.moveTo(pos.x, pos.y);
+      for (let i = 1; i < vehicle.points.length; i++) {
+        if (i == vehicle.points.length - 1) {
+          let pos = getCanvasPosition(vehicle.points[i]);
+          // if we are currently animating to the last point, draw the animation step instead
+          if (vehicle.animationTargets.length > 0) {
+            pos = getCanvasPosition(vehicle.animationTargets.shift());
+          }
+          ctx.arc(pos.x, pos.y, 3, 0, 2 * Math.PI);
+          ctx.fill();
+        } else {
+          // draw the next line segment
+          let pos = getCanvasPosition(vehicle.points[i]);
+          ctx.lineTo(pos.x, pos.y);
+        }
+      }
       ctx.stroke();
     }
-  }, 10000);
+  }, 16.67);
 </script>
 
 <map>
   {#if showMap}
     <h2>Map of the {lineName}</h2>
-    <canvas bind:this={canvas} width="1000" height="1000" />
+    <canvas
+      bind:this={canvas}
+      width={canvasDimensions.x}
+      height={canvasDimensions.y}
+    />
   {:else}
     <h2>Click to display the map</h2>
   {/if}
@@ -98,6 +205,11 @@
 <style>
   canvas {
     width: 100%;
-    height: 100%;
+    max-width: 100vh;
+  }
+  canvas:after {
+    content: "";
+    display: block;
+    padding-bottom: 100%;
   }
 </style>
